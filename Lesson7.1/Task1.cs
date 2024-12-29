066829aaf88c9412368752b2ec5fdb2cf3d2e03f
D1mastaSPAL <su66071n@mail.ru>
2024-12-29 17:08:02 +0300

    first commit

---
+++ Lesson7.1/Task1.cs
@@ -0,0 +1,33 @@
+﻿namespace Lesson7._1;
+
+internal class Task1
+{
+    static void Main(string[] args)
+    {
+        int[] ArrayInputConsole = new int[6];
+        Console.WriteLine($"Заполните одномерный массив {"\n"}");
+        int num = 1;
+        for (int i = 0; i < ArrayInputConsole.Length; i++)
+        {
+            Console.WriteLine($"Введите число {num++}: ");
+            var inputNumbers = Console.ReadLine();
+
+            if (int.TryParse(inputNumbers , out var count) && count > 0)
+            {
+                ArrayInputConsole[i] = count;
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод");
+                return;
+            }
+        }
+        Array.Sort(ArrayInputConsole);
+        Array.Reverse(ArrayInputConsole);
+        Console.Write($"Обратный порядок введенных вами чисел в отсортированном массиве: {"\n"}");
+        foreach (var outputNumbers in ArrayInputConsole)
+        {
+            Console.Write(outputNumbers + "\t");
+        }
+    }
+}
---
+++ Task2/Task2.cs
@@ -0,0 +1,40 @@
+﻿namespace Task2;
+
+internal class Task2
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix =
+            {
+                {-99, 19, 91,},
+                {77, -55, 75,},
+                {44, 43, -33,},
+            };
+        Console.Write("Двумерный массив: " + "\n");
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        IsTrueMethod(matrix);
+    }
+
+    public static bool IsTrueMethod(int[,] matrix)
+    {
+        int num = 0;
+        for (int y = 0; y < matrix.GetLength(1); y++)
+        {
+            num++;
+            int numberMax = 0;
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                if (y == 0 || numberMax < matrix[x, y]) numberMax = matrix[x, y];
+            }
+            Console.WriteLine($"Максимальное значение ряда {num}: {numberMax}");
+        }
+        return true;
+    }
+}
